c47f69ad9f1580833001f3c0132207f1d9f35944
monochromegane <dev.kuro.obi@gmail.com>
2014-01-05 22:46:19 +0900

    Added a test for find package.

---
+++ search/find/find_test.go
@@ -0,0 +1,23 @@
+package find
+
+import (
+	"github.com/monochromegane/the_platinum_searcher/search/grep"
+	"github.com/monochromegane/the_platinum_searcher/search/option"
+	"testing"
+)
+
+func TestFind(t *testing.T) {
+	out := make(chan *grep.Params)
+	finder := Finder{out, &option.Option{false, false}}
+	go finder.Find("../../files", "go")
+
+	for o := range out {
+		if o.Path == ".hidden/hidden.txt" {
+			t.Errorf("It should not contains file under hidden directory.")
+		}
+		if o.Path == "binary/binary.bin" {
+			t.Errorf("It should be text file.")
+		}
+	}
+
+}
