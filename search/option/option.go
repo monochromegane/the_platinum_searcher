27493a5ef20e1a667603c9c23c66764feecd3fcb
monochromegane <dev.kuro.obi@gmail.com>
2014-01-08 23:33:49 +0900

    Added a option for VCS Ignore files.

--- search/option/option.go
+++ search/option/option.go
@@ -1,6 +1,14 @@
 package option
 
 type Option struct {
-	NoColor bool `long:"nocolor" description:"Don't print color codes in results (Disabled by default)"`
-	NoGroup bool `long:"nogroup" description:"Don't print file name at header (Disabled by default)"`
+	NoColor   bool     `long:"nocolor" description:"Don't print color codes in results (Disabled by default)"`
+	NoGroup   bool     `long:"nogroup" description:"Don't print file name at header (Disabled by default)"`
+	VcsIgnore []string `long:"vsc-ignore" description:"VCS ignore files (Default: .gitignore)"`
+}
+
+func (self *Option) VcsIgnores() []string {
+	if len(self.VcsIgnore) == 0 {
+		self.VcsIgnore = []string{".gitignore"}
+	}
+	return self.VcsIgnore
 }
