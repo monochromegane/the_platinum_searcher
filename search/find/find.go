db553afa53f3358b37d82cf60f3d7ec8ba484155
monochromegane <dev.kuro.obi@gmail.com>
2014-01-08 22:28:15 +0900

    Ported 'Walk' method from filepath package.

--- search/find/find.go
+++ search/find/find.go
@@ -4,6 +4,7 @@ import (
 	"github.com/monochromegane/the_platinum_searcher/search/file"
 	"github.com/monochromegane/the_platinum_searcher/search/grep"
 	"github.com/monochromegane/the_platinum_searcher/search/option"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,7 +16,7 @@ type Finder struct {
 }
 
 func (self *Finder) Find(root, pattern string) {
-	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	Walk(root, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			if len(info.Name()) > 1 && strings.Index(info.Name(), ".") == 0 {
 				return filepath.SkipDir
@@ -31,4 +32,43 @@ func (self *Finder) Find(root, pattern string) {
 		return nil
 	})
 	close(self.Out)
+}
+
+type WalkFunc func(path string, info os.FileInfo, err error) error
+
+func Walk(root string, walkFn WalkFunc) error {
+	info, err := os.Lstat(root)
+	if err != nil {
+		return walkFn(root, nil, err)
+	}
+	return walk(root, info, walkFn)
+}
+
+func walk(path string, info os.FileInfo, walkFn WalkFunc) error {
+	err := walkFn(path, info, nil)
+	if err != nil {
+		if info.IsDir() && err == filepath.SkipDir {
+			return nil
+		}
+		return err
+	}
+
+	if !info.IsDir() {
+		return nil
+	}
+
+	list, err := ioutil.ReadDir(path)
+	if err != nil {
+		return walkFn(path, info, err)
+	}
+
+	for _, fileInfo := range list {
+		err = walk(filepath.Join(path, fileInfo.Name()), fileInfo, walkFn)
+		if err != nil {
+			if !fileInfo.IsDir() || err != filepath.SkipDir {
+				return err
+			}
+		}
+	}
+	return nil
 }
