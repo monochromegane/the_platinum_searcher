2c8e826186a97bf2404caae848912d649926bb5c
monochromegane <dev.kuro.obi@gmail.com>
2014-01-09 00:46:45 +0900

    Added a ignore package.

---
+++ search/ignore/ignore.go
@@ -0,0 +1,34 @@
+package ignore
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func IgnorePatterns(path string, ignores []string) []string {
+	var patterns []string
+	for _, ignore := range ignores {
+		file, err := os.Open(path + string(os.PathSeparator) + ignore)
+		if err != nil {
+			continue
+		}
+		reader := bufio.NewReader(file)
+		buf := make([]byte, 1024)
+
+		for {
+			buf, _, err = reader.ReadLine()
+			if err != nil {
+				break
+			}
+			s := strings.Trim(string(buf), " ")
+
+			if len(s) == 0 || strings.HasPrefix(s, "#") {
+				continue
+			}
+			patterns = append(patterns, s)
+		}
+		file.Close()
+	}
+        return patterns
+}
