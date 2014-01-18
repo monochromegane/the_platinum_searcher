c3cdad2d2feb66bc49f8cdf42de60d4b1b4d63fb
monochromegane <dev.kuro.obi@gmail.com>
2014-01-18 12:50:45 +0900

    Added a test that finds hidden dir when specified hidden dir.

--- search/find/find_test.go
+++ search/find/find_test.go
@@ -22,21 +22,38 @@ func TestFind(t *testing.T) {
 }
 
 var Ignores = []string{
-        "match/ignore.txt",
-        "ignore/ignore.txt",
-        "absolute/ignore.txt",
+	"match/ignore.txt",
+	"ignore/ignore.txt",
+	"absolute/ignore.txt",
 }
 
 func TestFindWithIgnore(t *testing.T) {
 	out := make(chan *grep.Params)
-        finder := Finder{out, &option.Option{VcsIgnore: []string{".vcsignore"}}}
+	finder := Finder{out, &option.Option{VcsIgnore: []string{".vcsignore"}}}
 	go finder.Find("../../files/vcs", "go")
 
 	for o := range out {
-                for _, ignore := range Ignores {
-                        if o.Path == "../../files/vcs/" + ignore {
-			        t.Errorf("It should not contains file.")
-                        }
-                }
+		for _, ignore := range Ignores {
+			if o.Path == "../../files/vcs/"+ignore {
+				t.Errorf("It should not contains file.")
+			}
+		}
+	}
+}
+
+func TestFindWhenSpecifiedHiddenDir(t *testing.T) {
+	out := make(chan *grep.Params)
+	finder := Finder{out, &option.Option{}}
+	go finder.Find("../../files/.hidden", "go")
+
+	found := false
+	for o := range out {
+		if o.Path == "../../files/.hidden"+"/hidden.txt" {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Errorf("It should find hidden dir when specified hidden dir.")
 	}
 }
