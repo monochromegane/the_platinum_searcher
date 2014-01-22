c7c3aa5a3ac10f57672ef75079cc4c5acc3516d7
yosssi <yoshida.keiji.84@gmail.com>
2014-01-22 18:22:57 +0900

    Created the test file for "option" package.

---
+++ search/option/option_test.go
@@ -0,0 +1,36 @@
+package option
+
+import (
+	"testing"
+)
+
+func TestVcsIgnores(t *testing.T) {
+	// When "VcsIgnore" is not specified
+	expected := []string{".gitignore", ".hgignore"}
+	option := Option{}
+	result := option.VcsIgnores()
+	if !sliceEqual(expected, result) || !sliceEqual(expected, option.VcsIgnore) {
+		t.Errorf("The result is invalid. [Expected: %v, Actual: %v]", expected, result)
+	}
+
+	// When "VcsIgnore" is specified
+	expected = []string{".foo", ".bar", ".baz"}
+	option = Option{VcsIgnore: expected}
+	result = option.VcsIgnores()
+	if !sliceEqual(expected, result) || !sliceEqual(expected, option.VcsIgnore) {
+		t.Errorf("The result is invalid. [Expected: %v, Actual: %v]", expected, result)
+	}
+}
+
+func sliceEqual(s1 []string, s2 []string) bool {
+	if len(s1) != len(s2) || cap(s1) != cap(s2) {
+		return false
+	}
+	for i, v1 := range s1 {
+		v2 := s2[i]
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
