dd5bb2644d3df4e14558cab37c5ea6d84eb67738
monochromegane <dev.kuro.obi@gmail.com>
2014-02-15 00:17:48 +0900

    Added a smart-case test.

---
+++ search/pattern/pattern_test.go
@@ -0,0 +1,25 @@
+package pattern
+
+import (
+	"testing"
+)
+
+type Assert struct {
+	Pattern   string
+	Expect    bool
+}
+
+func TestIgnoreCaseWithSmartCase(t *testing.T) {
+
+	asserts := []Assert{
+		Assert{"lowercase", true},
+		Assert{"Uppercase", false},
+	}
+
+        for _, assert := range asserts {
+                if NewPattern(assert.Pattern, true, true).IgnoreCase != assert.Expect {
+			t.Errorf("When pattern is %s, ignore case should be %t.", assert.Pattern, assert.Expect)
+                }
+        }
+
+}
