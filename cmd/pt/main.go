7ab76da1d5e2569e90b09435c5c0c3570187d779
monochromegane <dev.kuro.obi@gmail.com>
2015-12-05 14:19:33 +0900

    Specify GOMAXPROCS.

--- cmd/pt/main.go
+++ cmd/pt/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"os"
+	"runtime"
 
 	pt "github.com/monochromegane/the_platinum_searcher"
 )
+
+func init() {
+	if cpu := runtime.NumCPU(); cpu == 1 {
+		runtime.GOMAXPROCS(2)
+	} else {
+		runtime.GOMAXPROCS(cpu)
+	}
+}
 
 func main() {
 	pt := pt.PlatinumSearcher{Out: os.Stdout, Err: os.Stderr}
